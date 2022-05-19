b5f1d1b638ed662456277632d691a5d9729611b7
yeonnex <yeonnex@gmail.com>
2022-05-20 00:39:01 +0900

    User 엔티티 설계, UserRepository 생성

--- server/auth-service/src/main/java/com/example/authservice/api/repository/user/UserRepository.java
+++ server/auth-service/src/main/java/com/example/authservice/api/repository/user/UserRepository.java
@@ -1,4 +1,9 @@
 package com.example.authservice.api.repository.user;
 
-public class UserRepository {
+import com.example.authservice.api.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 }
---
+++ server/auth-service/src/main/java/com/example/authservice/oauth/ProviderType.java
@@ -0,0 +1,7 @@
+package com.example.authservice.oauth;
+
+public enum ProviderType {
+    GOOGLE,
+    KAKAO,
+    NAVER
+}
