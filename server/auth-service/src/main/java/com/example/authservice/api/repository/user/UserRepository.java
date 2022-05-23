713ec9e682c4a68a7b5d6532195ced2ceb86c786
yeonnex <yeonnex@gmail.com>
2022-05-23 22:48:09 +0900

    OAuth 로그인시 이메일로 기존 가입 회원 여부 확인할 것임

--- server/auth-service/src/main/java/com/example/authservice/api/repository/user/UserRepository.java
+++ server/auth-service/src/main/java/com/example/authservice/api/repository/user/UserRepository.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 데이터베이스에 이미 있는 이메일인지 체크하기 위한 메서드(즉, 이전에 가입한 사람인지 체크)
+    User findByEmail(String email);
 }
