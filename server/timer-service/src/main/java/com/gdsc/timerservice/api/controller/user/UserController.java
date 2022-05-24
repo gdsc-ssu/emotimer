c7971fb212a0eefac6f589d08d3903dd89aad746
yeonnex <yeonnex@gmail.com>
2022-05-24 14:34:41 +0900

    thymeleaf 세팅

--- server/timer-service/src/main/java/com/gdsc/timerservice/api/controller/user/UserController.java
+++ server/timer-service/src/main/java/com/gdsc/timerservice/api/controller/user/UserController.java
@@ -1,4 +1,14 @@
 package com.gdsc.timerservice.api.controller.user;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class UserController {
+    @GetMapping("/")
+    public String hello(){
+        return "index";
+    }
+
 }
