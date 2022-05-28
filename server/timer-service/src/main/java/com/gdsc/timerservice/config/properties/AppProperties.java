6f7e5a9841e586b910ac7bd66412f87739170417
yeonnex <yeonnex@gmail.com>
2022-05-28 23:29:58 +0900

    application.yml 설정값 클래스화

---
+++ server/timer-service/src/main/java/com/gdsc/timerservice/config/properties/AppProperties.java
@@ -0,0 +1,41 @@
+package com.gdsc.timerservice.config.properties;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+@Configuration
+public class AppProperties {
+    private final Auth auth = new Auth();
+    private final OAuth2 oAuth2 = new OAuth2();
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Auth {
+        private String tokenSecret;
+        private long tokenExpiry;
+        private long refreshTokenExpiry;
+    }
+
+    public static final class OAuth2 {
+        private List<String> authorizedRedirectUris = new ArrayList<>();
+        public List<String> getAuthorizedRedirectUris(){
+            return authorizedRedirectUris;
+        }
+        public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris){
+            this.authorizedRedirectUris = authorizedRedirectUris;
+            return this;
+        }
+    }
+
+}
