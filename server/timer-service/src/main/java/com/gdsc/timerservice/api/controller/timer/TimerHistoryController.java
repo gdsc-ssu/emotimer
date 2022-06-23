b3af4824061e0af4fdd974a4e492f45abb19de24
ohjinhokor <ohjinho7@gmail.com>
2022-06-23 14:39:30 +0900

    timer histroy skeleton

---
+++ server/timer-service/src/main/java/com/gdsc/timerservice/api/controller/timer/TimerHistoryController.java
@@ -0,0 +1,12 @@
+package com.gdsc.timerservice.api.controller.timer;
+
+import com.gdsc.timerservice.api.service.timer.TimerHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class TimerHistoryController {
+
+	private final TimerHistoryService timerHistoryService;
+}
