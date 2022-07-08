e3b4e4f73a62bd75270f6235e49959efa0c5b398
eric <eric@chai.finance>
2022-07-08 21:08:42 +0900

    chore: refactoring for support to client ouath login

--- server/timer-service/src/main/java/com/gdsc/timerservice/api/controller/auth/OAuthController.java
+++ server/timer-service/src/main/java/com/gdsc/timerservice/api/controller/auth/OAuthController.java
@@ -7,45 +7,50 @@ import com.gdsc.timerservice.oauth.exception.OAuthProviderMissMatchException;
 import com.gdsc.timerservice.oauth.handler.OAuth2AuthenticationSuccessHandler;
 import com.gdsc.timerservice.oauth.model.TokenResponse;
 import com.gdsc.timerservice.oauth.service.IssueRefreshService;
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class OAuthController {
 
-	private final OAuth2Service oAuth2Service;
-	private final IssueRefreshService issueRefreshService;
-	private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2Service oAuth2Service;
+    private final IssueRefreshService issueRefreshService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
-	/**
-	 * 사용자의 카카오 로그인 요청 후, 로그인한 다음 리다이렉트된 경로.<br/><br/> 카카오로 총 두번의 HTTP 요청으로 사용자 정보(이메일, 이름) 를 알아온다. <br/> 최종적으로 반환받은 사용자 정보로 강제 회원가입 절차를 진행한다. <br/><br/> 1. 로그인 성공 후 받은 쿼리파리미터의 code 를 이용해 카카오 인증 서버에 access token 요청<br/> 2. 받은 access token 을 가지고 카카오 리소스 서버에 사용자 정보 요청
-	 */
-	@GetMapping(value = "/callback/{vendor}", produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<?> codeFromKakao(@RequestParam String code,
-		@PathVariable String vendor) throws IOException {
-		// 강제 회원가입 시작
-		try {
-			User user = oAuth2Service.socialJoin(code, vendor);
-			TokenResponse tokenResponse = oAuth2Service.generateToken(user, user.getEmail(), RoleType.USER);
-			return ResponseEntity.ok()
-				.body(tokenResponse);
-		} catch (OAuthProviderMissMatchException e) {
-			return ResponseEntity.badRequest().body(e.getMessage());
-		}
-	}
+    /**
+     * 사용자의 카카오 로그인 요청 후, 로그인한 다음 리다이렉트된 경로.<br/><br/> 카카오로 총 두번의 HTTP 요청으로 사용자 정보(이메일, 이름) 를 알아온다. <br/> 최종적으로 반환받은 사용자 정보로 강제 회원가입 절차를 진행한다. <br/><br/> 1. 로그인 성공 후 받은 쿼리파리미터의 code 를 이용해 카카오 인증 서버에 access token 요청<br/> 2. 받은 access token 을 가지고 카카오 리소스 서버에 사용자 정보 요청
+     */
+    @GetMapping(value = "/callback/{vendor}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity codeFromKakao(@RequestParam String code, @PathVariable String vendor) throws IOException {
+        // 강제 회원가입 시작
+        try {
+            User user = oAuth2Service.socialJoin(code, vendor);
+            TokenResponse tokenResponse = oAuth2Service.generateToken(user, user.getEmail(), RoleType.USER);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.setLocation(UriComponentsBuilder.fromUriString("emotimer://success")
+                    .queryParam("accessToken", tokenResponse.getAccessToken())
+                    .queryParam("refreshToken", tokenResponse.getRefreshToken())
+                    .build().toUri());
+            return new ResponseEntity(httpHeaders, HttpStatus.SEE_OTHER);
+        } catch (OAuthProviderMissMatchException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
-	@GetMapping("/refresh")
-	public ResponseEntity refresh(HttpServletRequest request, HttpServletResponse response) {
-		issueRefreshService.refreshToken(request, response);
-		return ResponseEntity.ok().build();
-	}
+    @GetMapping("/refresh")
+    public ResponseEntity refresh(HttpServletRequest request) {
+        return ResponseEntity.ok(issueRefreshService.refreshToken(request));
+    }
 }
--- server/timer-service/src/main/java/com/gdsc/timerservice/oauth/service/IssueRefreshService.java
+++ server/timer-service/src/main/java/com/gdsc/timerservice/oauth/service/IssueRefreshService.java
@@ -1,25 +1,20 @@
 package com.gdsc.timerservice.oauth.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gdsc.timerservice.api.entity.user.UserRefreshToken;
 import com.gdsc.timerservice.api.repository.user.UserRefreshTokenRepository;
 import com.gdsc.timerservice.config.properties.AppProperties;
-import com.gdsc.timerservice.oauth.entity.RoleType;
 import com.gdsc.timerservice.oauth.model.TokenResponse;
 import com.gdsc.timerservice.oauth.token.AuthToken;
 import com.gdsc.timerservice.oauth.token.AuthTokenProvider;
 import com.gdsc.timerservice.util.HeaderUtil;
 import io.jsonwebtoken.Claims;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
-import java.util.Date;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,9 +25,7 @@ public class IssueRefreshService {
 	private final AuthTokenProvider tokenProvider;
 	private final UserRefreshTokenRepository userRefreshTokenRepository;
 
-	private final ObjectMapper objectMapper;
-
-	public void refreshToken(HttpServletRequest request, HttpServletResponse response) {
+	public Optional<TokenResponse> refreshToken(HttpServletRequest request) {
 		log.info("리프레시 토큰 발급 준비중...");
 
 		// refresh token 유효성 확인
@@ -41,19 +34,16 @@ public class IssueRefreshService {
 		Claims tokenClaims = authRefreshToken.getTokenClaims();
 
 		String email = tokenClaims.getSubject(); // 유저 이메일
-		RoleType roleType = RoleType.of(tokenClaims.get("role", String.class)); // 유저권한
 		if (!authRefreshToken.validate()) {
 			log.info("유효하지 않은 리프레시 토큰임.");
-			return;
-//            return ApiResponse.invalidRefreshToken(); // 유효하지 않다면 바로 response 리턴때림
+            return Optional.empty();
 		}
 
 		// email 과 refresh token 으로 DB 확인. 있다면 새로운 토큰 생성하여 응답
 		UserRefreshToken userRefreshToken = userRefreshTokenRepository.findByIdAndRefreshToken(Long.parseLong(tokenClaims.get("id").toString()), refreshToken);
 		if (userRefreshToken == null) {
 			log.info("해당하는 리프레시 토큰이 DB에 존재하지 않음");
-			return;
-//            return ApiResponse.invalidRefreshToken();
+            return Optional.empty();
 		}
 		Date now = new Date();
 		// 새로운 토큰 생성
@@ -61,16 +51,6 @@ public class IssueRefreshService {
 			email,
 			new Date(now.getTime() + appProperties.getAuth().getTokenExpiry()));
 		log.info("새로운 액세스 토큰 헤더에 탑재 완료!");
-		TokenResponse tokenResponse = new TokenResponse(accessToken.getToken(), userRefreshToken.getRefreshToken());
-		try {
-			response.setStatus(HttpServletResponse.SC_OK); // 200
-			response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-			PrintWriter writer = response.getWriter();
-			writer.write(objectMapper.writeValueAsString(tokenResponse));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-
+        return Optional.of(new TokenResponse(accessToken.getToken(), userRefreshToken.getRefreshToken()));
 	}
 }
