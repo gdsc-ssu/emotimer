4edd6e45ae8fcf3b5641b452fbee0ccfa3a9e506
eric <eric@chai.finance>
2022-07-08 20:53:34 +0900

    feat: add logout

--- client/lib/pages/settings-page.dart
+++ client/lib/pages/settings-page.dart
@@ -2,19 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gdsc_timer/shared/app_colors.dart';
 import 'package:gdsc_timer/shared/common-components.dart';
+import 'package:gdsc_timer/store/auth-store.dart';
+import 'package:provider/provider.dart';
 
 class Setting {
   String title;
   Widget? widget;
+  GestureTapCallback? onTap;
   bool hasArrow;
 
-  Setting(this.title, {this.widget = null, this.hasArrow = false});
+  Setting(this.title, {this.widget = null, this.hasArrow = false, this.onTap});
 }
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<AuthStore>(context);
+
     var settings = [
+      Setting("Email", widget: Text("yano24@naver.com")),
       Setting("Timer duration", widget: Text("50:00"), hasArrow: true),
       Setting("Timer auto start", widget: Switch(value: false, onChanged: (v) {})),
       Setting("Rest duration", widget: Text("15:00"), hasArrow: true),
@@ -24,6 +30,35 @@ class SettingsPage extends StatelessWidget {
       Setting("Help", hasArrow: true),
       Setting("Terms of service", hasArrow: true),
       Setting("Version", widget: Text("1.0.1")),
+      Setting("Singout", onTap: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [Text("Do you want to logout?")],
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text("Confirm"),
+                      onPressed: () async {
+                        await auth.logout();
+                        Navigator.pop(context);
+                        Navigator.pushReplacementNamed(context, "/login");
+                      },
+                    ),
+                    TextButton(
+                      child: Text("Cancel"),
+                      onPressed: () async {
+                        Navigator.pop(context);
+                      },
+                    )
+                  ]);
+            });
+      }),
     ];
 
     return Scaffold(
@@ -32,19 +67,20 @@ class SettingsPage extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         itemCount: settings.length,
         itemBuilder: (context, index) {
+          var setting = settings[index];
           var item = <Widget>[
-            Expanded(child: Text(settings[index].title, style: const TextStyle(color: AppColors.primary, fontSize: 16))),
+            Expanded(child: Text(setting.title, style: const TextStyle(color: AppColors.primary, fontSize: 16))),
           ];
 
-          if (settings[index].widget != null) {
+          if (setting.widget != null) {
             item.add(Expanded(
                 child: Align(
               alignment: Alignment.centerRight,
-              child: settings[index].widget,
+              child: setting.widget,
             )));
           }
 
-          if (settings[index].hasArrow) {
+          if (setting.hasArrow) {
             item.add(const Padding(
               padding: EdgeInsets.only(left: 8),
               child: Icon(Icons.arrow_forward_ios),
@@ -54,11 +90,11 @@ class SettingsPage extends StatelessWidget {
             visualDensity: const VisualDensity(vertical: -4),
             title: SizedBox(
               height: 40,
-              child: Row(children: item,),
+              child: Row(
+                children: item,
+              ),
             ),
-            onTap: () {
-              // Navigator.pushNamed(context, "/setting/$index");
-            },
+            onTap: setting.onTap,
           );
         },
         separatorBuilder: (context, index) {
